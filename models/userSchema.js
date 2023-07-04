5dc7f9b5a5b2f3fd0d996528867a7117f954ef0f
aloknath <aloknath.ar@gmail.com>
2023-07-04 23:16:00 +0530

    password hashing added

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bCrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -26,5 +27,14 @@ const userSchema = new mongoose.Schema({
         required:true
     }
 })
+userSchema.pre('save', async function(next){
+    if(this.isModified('password')){
+        console.log("Inside ismodified condition", bCrypt.hash(this.password, 12));
+        this.password = await bCrypt.hash(this.password, 12);
+        this.cpassword = await bCrypt.hash(this.cpassword, 12)
+    }
+    console.log(this.password);
+    next();
+});
 const User = mongoose.model('USER',userSchema)
 module.exports = User;
--- router/auth.js
+++ router/auth.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bCrypt = require('bcryptjs')
 require ('../db/connection')
 const User = require('../models/userSchema')
 const router = express.Router();
@@ -14,14 +15,17 @@ router.post('/register', async (req, res) =>{
     .then((userExists) => {
         if(userExists){
             return res.status(422).json({message:"User Already Exists", status:"error"})
+        }else if(password != cpassword){
+            return res.status(422).json({message:"Passwords dosen't match", status:"error"})
+        }else{
+            const user = new User({name, email, phone, work, password, cpassword})
+            user.save().then(()=>{
+                return res.status(200).json({status:"Data saved successfuly", remarks:"success"})
+            })
+            .catch((error) => {
+                return res.status(500).json({status:"Failed To Register",error:{type:error.name, message:error.message}, remarks:"error"})
+            })
         }
-        const user = new User({name, email, phone, work, password, cpassword})
-        user.save().then(()=>{
-            return res.status(200).json({status:"Data saved successfuly", remarks:"success"})
-        })
-        .catch((error) => {
-            return res.status(500).json({status:"Failed To Register",error:{type:error.name, message:error.message}, remarks:"error"})
-        })
     })
     .catch((error) => {
         console.log({error});
@@ -32,11 +36,12 @@ router.post('/register', async (req, res) =>{
 // Login API
 router.post('/signin', async (req, res) => {
     try{
-        const { email, name } = req.body;
-        if(!email || !name){
+        const { email, password } = req.body;
+        if(!email || !password){
             return res.status(422).json({message:"You missed some data", status:"error"})
         }
         const loginUserStatus = await User.findOne({email:email});
+        const isMatched = bCrypt.compare(password, password.password);
         if(loginUserStatus){
             return res.status(200).json({status:"Logged-in successfuly", remarks:"success"})
         }else{
